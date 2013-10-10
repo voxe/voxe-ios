25228caec972dd4c8730d824dc55a7f224366317
koheku <phkoeck@gmail.com>
2013-10-10 17:06:10 +0200

    Button with voxe logo

--- app/controllers/propositions_view_controller.rb
+++ app/controllers/propositions_view_controller.rb
@@ -4,10 +4,33 @@ class PropositionsViewController < UIViewController
 
   def viewDidLoad
     view.backgroundColor = '#E7ECEE'.to_color
+
+    # Set logo on navigation bar
+    logoButton = UIButton.buttonWithType(UIButtonTypeCustom)
+    logoButton.frame = [[0,0],[32,32]]
+    logoButton.setImage(UIImage.imageNamed("voxelogo.png"), forState:UIControlStateNormal)
+    logoButton.addTarget(self, action:'logoButtonPressed', forControlEvents:UIControlEventTouchUpInside)
+    navigationItem.titleView = logoButton
+
+    # Set webview
     @webView = layout(UIWebView, :webView)
     @webView.delegate = self
     self.view = @webView
+
+    # Set toolbar with back button
+    navigationController.setToolbarHidden(false, animated:false)
+    backItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(101, target:@webView, action:'goBack')
+    backItem.style = UIBarButtonItemStyleBordered
+    setToolbarItems([*backItem], animated:false)
   end
+
+  # Interface buttons
+
+  def logoButtonPressed
+    p 'test'
+  end
+
+  # UIWebView methods
 
   def webViewDidFinishLoad(webView)
     MBProgressHUD.hideHUDForView(self.view, animated:true)
